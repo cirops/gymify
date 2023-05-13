39bd2c0ad3a8c6cf5cad58e9c9f634ef14a44582
cirops <cirops@gmail.com>
2023-05-13 18:17:44 -0300

    feat: applies dependency inversion principle through interfaces

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
-import { registerUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -11,8 +12,12 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const usersRepository = new PrismaUsersRepository()
+
+  const registerUseCase = new RegisterUseCase(usersRepository)
+
   try {
-    await registerUseCase({ name, email, password })
+    await registerUseCase.execute({ name, email, password })
   } catch (err) {
     return reply.status(409).send()
   }
