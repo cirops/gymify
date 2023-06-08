a178c207a07f4c02cbd0a71c231d2fec7d53abdc
cirops <cirops@gmail.com>
2023-06-08 16:28:07 -0300

    test: gyms e2e tests

---
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,72 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('List Nearby Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to list nearby gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    // await gymsRepository.create({
+    //   title: 'Near Gym',
+    //   description: null,
+    //   phone: null,
+    //   latitude: -27.5853523,
+    //   longitude: -48.5085744,
+    // })
+
+    // await gymsRepository.create({
+    //   title: 'Far Gym',
+    //   description: null,
+    //   phone: null,
+    //   latitude: -27.5853523,
+    //   longitude: -49.4917664,
+    // })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description',
+        phone: '11999999999',
+        latitude: -27.5853523,
+        longitude: -48.5085744,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'TypeScript Gym',
+        description: 'Some description',
+        phone: '11999999999',
+        latitude: -27.5853523,
+        longitude: -49.4917664,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: -27.5853523,
+        longitude: -48.5085744,
+      })
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'JavaScript Gym',
+      }),
+    ])
+  })
+})
--- src/use-cases/fetch-nearby-gyms.spec.ts
+++ src/use-cases/fetch-nearby-gyms.spec.ts
@@ -37,27 +37,4 @@ describe('Search Gyms Use Case', () => {
     expect(gyms).toHaveLength(1)
     expect(gyms).toEqual([expect.objectContaining({ title: 'Near Gym' })])
   })
-
-  // it('should be able to fetch paginated gyms search', async () => {
-  //   for (let i = 1; i <= 22; i += 1) {
-  //     await gymsRepository.create({
-  //       title: `JavaScript Gym ${i}`,
-  //       description: null,
-  //       phone: null,
-  //       latitude: -27.5853523,
-  //       longitude: -48.5085744,
-  //     })
-  //   }
-
-  //   const { gyms } = await sut.execute({
-  //     query: 'JavaScript',
-  //     page: 2,
-  //   })
-
-  //   expect(gyms).toHaveLength(2)
-  //   expect(gyms).toEqual([
-  //     expect.objectContaining({ title: 'JavaScript Gym 21' }),
-  //     expect.objectContaining({ title: 'JavaScript Gym 22' }),
-  //   ])
-  // })
 })
